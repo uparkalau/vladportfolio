dd43baa91c5e56d6c6f2e536749c187e102d8f4f
coinbread <113056897+coinbread@users.noreply.github.com>
2023-08-08 14:24:13 -0700

    navbar for projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,15 +1,31 @@
 import ProjectCard from "@/components/ProjectCard";
-import { projects } from "@/data";
+import ProjectsNavbar from "@/components/ProjectsNavbar";
+import { projects as projectsData } from "@/data";
+import { Category } from "@/type";
+import { useState } from "react";
 
 
 const Projects = () => {
+  const [projects, setProjects] = useState(projectsData)
+  const [active, setActive] = useState("all")
+  const handleFilterCategory = (category: Category | 'all') => {
+    if(category === "all"){
+      setProjects(projectsData)
+      setActive(category)
+      return
+    }
+    const newProjectsData = projectsData.filter((project) => project.category.includes(category))
+    setProjects(newProjectsData)
+    setActive(category)
+  }
+
   return (
     <div className="px-5 py-2" style={{height: '65vh'}}>
-      <nav>Navbar</nav>
+      <ProjectsNavbar handleFilterCategory = {handleFilterCategory} active={active}/>
       <div className="relative grid grid-cols-12 gap-4 my-3">
         {projects.map((project)=>(
           <>
-            <div className="col-span-12 p-2 sm:col-span-6 lg:col-span-4 bg-gray-200 dark:bg-dark-200 rounded-lg">
+            <div className="col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-4 dark:bg-dark-200">
               <ProjectCard project={project} key={project.name}/>
             </div>
           </>
